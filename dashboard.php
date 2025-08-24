667f1d68063722e6acc0823e3ee3a0a6d3e2bc21
Jasmine Santiago <159919413+prettysunset@users.noreply.github.com>
2025-08-24 17:09:12 +0800

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="sample.php" style="padding:8px 15px; background:#4CAF50; color:white; text-decoration:none; border-radius:4px;">ROOM106
+        
+    </a>
+
+</body>
+</html>
