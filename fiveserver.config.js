cb3bdcf3e485b98e10947e9d32badb8bd5c6c029
Jasmine Santiago <santiagojasminem@gmail.com>
2025-10-11 11:19:02 +0800

    ayaw gumana ng live server

---
+++ fiveserver.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  php: "C:\\xampp\\php\\php.exe",
+  root: "C:\\xampp\\htdocs\\capstone_sample\\CapstoneSample",
+  port: 5500,
+  open: true,
+  phpIni: "C:\\xampp\\php\\php.ini"
+};
