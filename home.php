dc3d83b91c670a3b6cc13c3c0e2ed7927af899b8
jaime0123 <150927283+jaime0123@users.noreply.github.com>
2025-10-12 13:06:43 +0800

    homepagePartial

---
+++ home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="stylenibilog.css" />
+    <title>OJT-MS HOME</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap" rel="stylesheet">
+
+</head>
+<body>    
+
+    <div class="navbar">
+        <h1><a class="logo" href="about.php">OJT-MS</a></h1>
+
+        <div class="nav-links">
+        <li class="home">Home</li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contacts.php">Contacts</a></li>
+        <li><a href="offices.php">Offices</a></li>
+        <li class="login"><a href="login.php">Login</a></li>
+        </div>
+    </div>
+    <div class="header-container" style="background-image: url('img_cityhall.png'); background-size: cover; background-position: center; padding: 60px 0;">
+        <p style="font-size: 65px; margin-top: 0;" class="header"     text-shadow: 5px 5px white;>Welcome to the<br> Malolos City - OJT <br>Management <br>System</p>
+        <p class="subheader">Your Pathway to Growth and <br>Service starts here</p>
+
+        <div class="buttons">
+            <button class="mainbtn"> <a style="color:white; text-decoration:none"href="why.html ">Apply Now</a></style></button><br>
+            <button class="secondbtn"><a style="color:#3a4163; text-decoration:none" href="requirements.html">Requirements</a> </button>
+        </div>
+
+        <div>
+            <p class="date" style="margin-left: 1350px; font-weight: bolder; color: #3a4163; font-size: 15px; padding: 0; background-color: #e6f2ff; display: inline-block;">October 12, 2025</p>
+        </div>
+
+    </div>
+    
+    
+
+      
+       
+
+
+
+</body>
+</html>
